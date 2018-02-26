f05ef9f94b2c7be900f9dc776b8184bae230f4ea
Spoverud <spoverud@css.edu>
2018-02-25 19:17:05 -0600

    Participation activity unit 6

--- app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -1,6 +1,8 @@
 package css.cis3334.intentsperformances;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     private void setupButtonClickEvents() {
         /**
          *   Set up button click event listener for the web info button for the first performance
@@ -37,6 +39,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Do something in response to button click
                 textViewStatus.setText("Code should display website for performance 1");
+
+                Uri webpage = Uri.parse("http://www.android.com");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(webIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                boolean isIntentSafe = activities.size() > 0;
+
+                if (isIntentSafe) {
+                    startActivity(webIntent);
+                }
             }
         });
 
@@ -72,10 +86,167 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Do something in response to button click
                 textViewStatus.setText("Code should display map for performance 1");
+
+                // Map point based on address
+                Uri location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(mapIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                boolean isIntentSafe = activities.size() > 0;
+
+                if (isIntentSafe) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
+    }
+
+    private void setupButtonClickEvents2() {
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn1Web = (Button) findViewById(R.id.button2WebInfo);
+        btn1Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display website for performance 2");
+
+                Uri webpage = Uri.parse("http://www.android.com");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(webIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                boolean isIntentSafe = activities.size() > 0;
+
+                if (isIntentSafe) {
+                    startActivity(webIntent);
+                }
             }
         });
 
+        /**
+         *   Set up button click event listener for the celendar button for the first performance
+         *       Use the calendar intent to set an event reminder on the calendar
+         */
+        but1Calendar = (Button) findViewById(R.id.button2Calendar);
+        but1Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 2");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "SAL Colloquium 'The Robot Next Door'");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startActivity(calIntent);
 
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn1Map = (Button) findViewById(R.id.button2map);
+        btn1Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display map for performance 2");
+
+                // Map point based on address
+                Uri location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(mapIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                boolean isIntentSafe = activities.size() > 0;
+
+                if (isIntentSafe) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
+    }
+
+    private void setupButtonClickEvents3() {
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn1Web = (Button) findViewById(R.id.button3WebInfo);
+        btn1Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display website for performance 1");
+
+                Uri webpage = Uri.parse("http://www.android.com");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(webIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                boolean isIntentSafe = activities.size() > 0;
+
+                if (isIntentSafe) {
+                    startActivity(webIntent);
+                }
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the celendar button for the first performance
+         *       Use the calendar intent to set an event reminder on the calendar
+         */
+        but1Calendar = (Button) findViewById(R.id.button3Calendar);
+        but1Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 1");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "SAL Colloquium 'The Robot Next Door'");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startActivity(calIntent);
+
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn1Map = (Button) findViewById(R.id.button3map);
+        btn1Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display map for performance 1");
+
+                // Map point based on address
+                Uri location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(mapIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                boolean isIntentSafe = activities.size() > 0;
+
+                if (isIntentSafe) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
     }
 
 
